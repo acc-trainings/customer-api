8dde667ae4014612b8bd3fb6b83915fc96b99bcb
cshum-acc <78983575+cshum-acc@users.noreply.github.com>
2021-02-19 17:36:01 -0500

    Connect to MongoDB

--- src/main/java/com/acc/training/customerapi/repository/CustomerRepository.java
+++ src/main/java/com/acc/training/customerapi/repository/CustomerRepository.java
@@ -1,38 +1,15 @@
 package com.acc.training.customerapi.repository;
 
-import java.util.HashMap;
-import java.util.Map;
 
-import com.acc.training.customerapi.model.Customer;
+import com.acc.training.customerapi.domain.CustomerDomain;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-@Repository
-public class CustomerRepository {
+
+public interface CustomerRepository extends MongoRepository<CustomerDomain, String > {
+  
+    public CustomerDomain findOneByCustomerId(String CustomerId);
+
     
-    private static final Map<String, Customer> customerMap = new HashMap<>();
-
-    static{
-        initCustomer();
-    }
-
-    private static void initCustomer() {
-        Customer cust1 = new Customer();
-        cust1.setCustomerId("12345");
-        cust1.setCustomerName("Chris Shum");
-        cust1.setCustomerAddress("Womp, WO");
-
-        customerMap.put("12345", cust1);
-    }
-
-    public Customer getCustomer(String custId){
-        return customerMap.get(custId);
-
-    }
-    
-    public Customer addCustomer(Customer customer){
-        customerMap.put(customer.getCustomerId(), customer);
-        return customer;
-    }
 
 }
