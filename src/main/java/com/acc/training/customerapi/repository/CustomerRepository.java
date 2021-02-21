33fef52a220771a3945b21b4c06b5cfb4ccfaeb3
sandeepp873 <69438787+sandeepp873@users.noreply.github.com>
2021-02-21 18:21:11 -0500

    Mongodb code

--- src/main/java/com/acc/training/customerapi/repository/CustomerRepository.java
+++ src/main/java/com/acc/training/customerapi/repository/CustomerRepository.java
@@ -1,38 +1,13 @@
 package com.acc.training.customerapi.repository;
 
-import java.util.HashMap;
-import java.util.Map;
 
-import com.acc.training.customerapi.model.Customer;
+import com.acc.training.customerapi.domain.CustomerDomain;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-import org.springframework.stereotype.Repository;
 
-@Repository
-public class CustomerRepository {
 
-    private static final Map<String, Customer> customerMap = new HashMap<>();
+public interface CustomerRepository extends MongoRepository<CustomerDomain, String> {
 
-    static{
-        initCustomer();
-    }
-
-    private static void initCustomer() {
-        Customer cust1 = new Customer();
-        cust1.setCustomerId("12345");
-        cust1.setCustomerName("Rahul Anand");
-        cust1.setCustomerAddress("Hartford, CT");
-
-        customerMap.put("12345", cust1);
-    }
-
-    public Customer getCustomer(String custId){
-        return customerMap.get(custId);
-
-    }
-
-    public Customer addCustomer(Customer customer){
-        customerMap.put(customer.getCustomerId(), customer);
-        return customer;
-    }
-    
+    public CustomerDomain findByCustomerId(String CustomerId);
+   
 }
